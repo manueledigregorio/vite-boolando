329ea4fe292d6fbaff7d7a5199cfac4379d7505e
manueledigregorio <dig.manuele@gmail.com>
2023-10-10 18:31:21 +0200

    [ADD] Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,10 +5,54 @@ export default {
 </script>
 
 <template>
-  
+  <footer>
+        <div class="container  ">
+
+            <div>
+                <h3>Booleando S.r.l.</h3>
+                <span>Informazioni legali</span>
+                <span>informativa sulla privacy</span>
+                <span>Diritto di recesso</span>
+            </div>
+            <div>
+                <span>Trovaci anche su</span>
+                <ul>
+                    <li><a><i class="fa-brands fa-twitter"></i></a></li>
+                    <li><a><i class="fa-brands fa-square-facebook"></i>
+                    </a></li>
+                    <li><a><i class="fa-brands fa-instagram"></i></a></li>
+                    <li><a><i class="fa-brands fa-pinterest"></i></a></li>
+                    <li><a><i class="fa-brands fa-youtube"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </template>
 
 
-<style>
+<style lang="scss" scoped>
+ @use '../scss/partials/General.scss' as *;
+  @use '../scss/partials/typography.scss' as *;
+  @use '../scss/partials/variabiles.scss' as *;
 
+  footer{
+    background-color:$black ;
+    padding-top:40px ;
+    padding-bottom: 30px;
+    color: $white;
+
+    .container{
+      display: flex;
+      justify-content: space-between;
+      align-items: start;
+    }
+    ul{
+      display: flex;
+    }
+
+    li{
+      margin: 7px;
+    }
+   
+  }
 </style>
